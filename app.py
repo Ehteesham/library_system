c039419074628dae877c98dfd0e631a333b9484e
ehtessham <ehteshaam44>
2023-09-18 23:58:30 +0530

    remove some notebooks and update app.py

--- app.py
+++ app.py
@@ -3,52 +3,56 @@ import tkinter as tk
 from inventory_management_system import InventoryMangement
 from login import LoginPage
 
-# Initializing an object
-root = tk.Tk()
-
-# Adjusting tkinter window
-root.title("Library Management System")
 
 
-# Top Left Frame
-book_list_frame = tk.Frame(root, width=970, height=350, bg="white")
-book_list_frame.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
 
-# Top Right Frame
-login_frame = tk.Frame(root, width=510, height=150, bg="white")
-login_frame.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
+if __name__ == '__main__':
+    # Initializing an object
+    root = tk.Tk()
 
-# Bottom Left Box
-lower_left = tk.Frame(root, width=200, height=200, bg="white")
-lower_left.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
-lower_left.pack_propagate(False)
+    # Adjusting tkinter window
+    root.title("Library Management System")
 
 
-# Bottom Right Box
-lower_right = tk.Frame(root, width=200, height=200, bg="white")
-lower_right.grid(row=1, column=1, columnspan=2, padx=5, pady=5, sticky="nsew")
+    # Top Left Frame
+    book_list_frame = tk.Frame(root, width=970, height=350, bg="white")
+    book_list_frame.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
 
-# Top Right Buttons
-lg = LoginPage(root, book_list_frame)
+    # Top Right Frame
+    login_frame = tk.Frame(root, width=510, height=150, bg="white")
+    login_frame.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
 
-# Login Button
-login_button = tk.Button(
-    login_frame,
-    width=28,
-    height=2,
-    text="Login",
-    command=lambda: lg.login_(login_frame, lower_right, lower_left),
-)
-login_button.pack(padx=2, pady=5, anchor="center")
+    # Bottom Left Box
+    lower_left = tk.Frame(root, width=200, height=200, bg="white")
+    lower_left.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+    lower_left.pack_propagate(False)
 
-# New User Registration Button
-registration_button = tk.Button(
-    login_frame,
-    width=28,
-    height=2,
-    text="Register",
-    command=lambda: lg.user_registration(login_frame),
-)
-registration_button.pack(pady=5, anchor="center")
 
-root.mainloop()
+    # Bottom Right Box
+    lower_right = tk.Frame(root, width=200, height=200, bg="white")
+    lower_right.grid(row=1, column=1, columnspan=2, padx=5, pady=5, sticky="nsew")
+
+    # Top Right Buttons
+    lg = LoginPage(root, book_list_frame)
+
+    # Login Button
+    login_button = tk.Button(
+        login_frame,
+        width=28,
+        height=2,
+        text="Login",
+        command=lambda: lg.login_(login_frame, lower_right, lower_left),
+    )
+    login_button.pack(padx=2, pady=5, anchor="center")
+
+    # New User Registration Button
+    registration_button = tk.Button(
+        login_frame,
+        width=28,
+        height=2,
+        text="Register",
+        command=lambda: lg.user_registration(login_frame),
+    )
+    registration_button.pack(pady=5, anchor="center")
+
+    root.mainloop()
